404b44bb96bc06c315e24d831a5bbf1e0624e175
WALL-E <WALL-E@myself.com>
2022-11-17 15:28:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author 南风
+ * @notes
+ * @create -11-17-15:23
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
