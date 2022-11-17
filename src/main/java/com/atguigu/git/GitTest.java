32d9df654cb763f34aed7d750673b2be01d4138d
WALL-E <WALL-E@myself.com>
2022-11-17 16:11:22 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello,hot-fix");
         System.out.println("hello,git3");
         System.out.println("hello,hot-fix2");
+        System.out.println("push,test");
         }
 }
